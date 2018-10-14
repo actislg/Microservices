395bc02d5d392a722b9c5eee11bee927bb176690
gabriel <gactisaversa@janpak.com>
2018-10-14 14:28:27 -0400

    initial commit

---
+++ my-nodejs-service/server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app =express();
+app.get('/',(req,res)=>{
+    res.send('Hello from App Engine');
+});
+
+//listen to teh app engine in port 8080
+const PORT=process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}...`);
+});
+
