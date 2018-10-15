12bb542e856ad0a437988ccdee3c5416a6ea2e5b
gabriel <gactisaversa@janpak.com>
2018-10-15 09:25:33 -0400

    fix error

--- my-nodejs-service/server.js
+++ my-nodejs-service/server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app =express();
 app.get('/',(req,res)=>{
-    res.send('Hello from App Engine v 1.00 ...');
+    res.send('Hello from App Engine v 1.00 ...ps');
 });
 
 //listen to the app engine in port 8080
@@ -9,5 +9,3 @@ const PORT=process.env.PORT || 8080;
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}...`);
 });
-
-s
