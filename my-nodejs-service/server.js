94128c46496060e7f179370f7762bf6fc92fc302
gabriel <gactisaversa@janpak.com>
2018-10-15 08:55:05 -0400

    added app yaml

--- my-nodejs-service/server.js
+++ my-nodejs-service/server.js
@@ -4,7 +4,7 @@ app.get('/',(req,res)=>{
     res.send('Hello from App Engine');
 });
 
-//listen to teh app engine in port 8080
+//listen to the app engine in port 8080
 const PORT=process.env.PORT || 8080;
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}...`);
